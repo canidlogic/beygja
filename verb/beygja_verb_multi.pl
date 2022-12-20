8db2955f824032bf5c63359ddc63a580015bbff8
Noah Johnson <noah.johnson@loupmail.com>
2022-12-20 10:32:40 -0600

    Use new Util module functions

--- verb/beygja_verb_multi.pl
+++ verb/beygja_verb_multi.pl
@@ -6,9 +6,7 @@ use utf8;
 # Beygja imports
 use BeygjaConfig qw(beygja_dbpath);
 use Beygja::DB;
-
-# Core imports
-use Unicode::Collate::Locale;
+use Beygja::Util qw(verbParadigm wordList);
 
 =head1 NAME
 
@@ -21,19 +19,15 @@ variants.
 
 =head1 DESCRIPTION
 
-Go through each verb in the verb database word table and get the
-headword.  Only verbs in the core set with a default grade are
-considered.
+Go through each verb in the core set defined by the C<wordList()>
+function of C<Beygja::Util>.
 
-For each verb, go through all the associated inflections that have a
-default grade.  It any inflection code has more than one form with a
-default code, then the verb has inflectional variants.
+For each verb, fetch the paradigm using the C<verbParadigm()> function
+in the C<Beygja::Util> module.
 
-Compile a list of all verbs with inflectional variants.  Sort this list
-according to the Icelandic locale and print it out.
-
-For each verb on the list, report all of the inflection codes that have
-variants.
+Compile a list of all verbs that have inflectional variants in their
+paradigms.  Print this list out, along with details for each verb
+regarding what the variants are.
 
 The location of the verb database is determined by C<BeygjaConfig.pm>
 
@@ -57,104 +51,53 @@ my $dbc = Beygja::DB->connect(beygja_dbpath('verb'), 0);
 
 # Perform a work block that will contain all operations
 #
-my $dbh = $dbc->beginWork('r');
-my $sth;
-my $qr;
+$dbc->beginWork('r');
 
-# Figure out the default grade
+# Get the core wordlist
 #
-$qr = $dbh->selectrow_arrayref("SELECT gid FROM grade WHERE giv = 1");
-(defined $qr) or die "Can't find default grade!\n";
-my $default_grade = $qr->[0];
+my @wlist = wordList($dbc, "core");
 
-# Get an array of all core, default-grade verb headwords and their word
-# keys
+# Compile the list of verbs with variants
 #
-my @verb_array;
-
-$sth = $dbh->prepare(
-  "SELECT wlem, wid FROM word WHERE gid = ? AND wvs = 0");
-$sth->bind_param(1, $default_grade);
-$sth->execute;
-for(my $rec = $sth->fetchrow_arrayref;
-    defined $rec;
-    $rec = $sth->fetchrow_arrayref) {
-  push @verb_array, ([
-    Beygja::DB->dbToString($rec->[0]),
-    $rec->[1]
-  ]);
-}
-
-# Go through the array and compile the list of verbs with inflectional
-# variants
-#
-my @multi_verbs;
-
-for my $vrec (@verb_array) {
+for my $vrec (@wlist) {
   # Get verb fields
   my $verb_lem = $vrec->[0];
   my $verb_wid = $vrec->[1];
   
-  # Start the inflection map empty and the multi-field map empty, and
-  # clear the multi_flag to begin with; imap maps inflection codes to
-  # the first form read; fmap maps inflection codes to an array of two
-  # or more forms, including the first one that was read
-  my $imap = {};
-  my $fmap = {};
-  my $multi_flag = 0;
+  # Conjugate this verb
+  my %conj = verbParadigm($dbc, $verb_wid);
   
-  # Go through all inflections for this word that have a default grade
-  $qr = $dbh->selectall_arrayref(
-        "SELECT icode, iform FROM infl WHERE wid = ? AND gid = ?",
-        undef,
-        $verb_wid, $default_grade);
-  if (defined $qr) {
-    for my $rec (@$qr) {
-      # Get current textual code and inflectional form
-      my $icode = $rec->[0];
-      my $iform = Beygja::DB->dbToString($rec->[1]);
-      
-      # Check whether textual code already defined for this verb
-      if (defined $imap->{$icode}) {
-        # Already defined, so set the multi_flag and add this field to
-        # the variant field set if not already there
-        $multi_flag = 1;
-        unless (defined $fmap->{$icode}) {
-          $fmap->{$icode} = [$imap->{$icode}];
-        }
-        push @{$fmap->{$icode}}, ($iform);
-        
-      } else {
-        # Not defined, so add it to the map
-        $imap->{$icode} = $iform;
+  # The verb report flag starts out clear
+  my $verb_reported = 0;
+  
+  # Go through all conjugated fields and look for variants
+  for my $k (sort keys %conj) {
+    # Only do something with key if it has variants
+    if (ref($conj{$k})) {
+      # Variants found, so if verb hasn't been reported yet, report the
+      # verb headword and set the verb reported flag
+      unless ($verb_reported) {
+        $verb_reported = 1;
+        printf "%s\n", $verb_lem;
       }
+      
+      # Report this field name
+      printf "  %s -", $k;
+      
+      # Report all variant values for this field
+      for my $fm (@{$conj{$k}}) {
+        print " $fm";
+      }
+      
+      # Finish the line
+      print "\n";
     }
-  }
-  
-  # If multi_flag set, add this verb and its mapping of inflection codes
-  # to the multi_verb array
-  if ($multi_flag) {
-    push @multi_verbs, ([$verb_lem, $fmap]);
   }
 }
 
 # If we got here, finish the work block successfully
 #
 $dbc->finishWork;
-
-# Sort the selected verbs in Icelandic style and print them
-#
-my $col = Unicode::Collate::Locale->new(locale => 'is');
-for my $vb (sort { $col->cmp($a->[0], $b->[0]) } @multi_verbs) {
-  printf "%s\n", $vb->[0];
-  for my $vf (sort keys %{$vb->[1]}) {
-    printf "  %s -", $vf;
-    for my $fm (@{$vb->[1]->{$vf}}) {
-      print " $fm";
-    }
-    print "\n";
-  }
-}
 
 =head1 AUTHOR
 
